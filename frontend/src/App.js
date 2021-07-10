a01ad6c6d3181466aaa990be8c634d970639223e
Mohanraj <77585218+mohanrajreese@users.noreply.github.com>
2021-07-10 15:01:48 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import HomePage from './components/HomePage'
 import { ActiveSite } from './index'
 import Container from 'react-bootstrap/Container'
 
+
 let App= () => {
 
     const site=useContext(ActiveSite)
